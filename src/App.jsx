e2b08cfab1aa52e8ae17bd14e67a987735e68ec3
jose brenon <eibrenon@gmail.com>
2024-07-23 02:37:16 -0300

    Criando o repo do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>Lista de Contatos - React</h1>
+      <hr />
+    </>
+  );
+}
